2c9563af0cc435323c91aedc4c231f6fcb9ba22b
Shamim <74086860+Deathshotterdie98@users.noreply.github.com>
2021-12-08 00:09:58 +0600

    Update index.php

--- index.php
+++ index.php
@@ -5,53 +5,23 @@ session_start();
 <!DOCTYPE html>
 <html>
 <head>
-  <title></title>
+
+  <div class="topnav">
+  <a class="active" href="index.php">Home</a>
+  <a href="New%20addition/contact_us_email_enabled/contact_us.php">Contact</a>
+  <a href="about_us.php">About</a>
+  <a href="signup.php">Sign Up</a>
+  <a href="login.php">Login</a>
+</div>
+
   <div class="banner-text">
     <h1>Car Sharing</h1>
-    <p>Plan your next trip now</p>
+    <p>Plan your next trip now
   <link rel="stylesheet" type="text/css" href="css/index.css">
-  <link rel="preconnect" href="https://fonts.gstatic.com">
-<link href="https://fonts.googleapis.com/css2?family=Kalam&family=Kaushan+Script&display=swap" rel="stylesheet">
 </head>
 <body>
-  <br><br><br><br><br>
-  <div class="banner-btn">
-    <a href="login.php">Login</a>
-    <a href="signup.php">Sign Up</a>
-  </div>
+ 
 
-  <div class="banner-btn">
-  <div id="sideNav">
-    <nav>
-      <ul>
-        <li><a href="index.php">Home</a></li>
-        <li><a href="about_us.php">About Us</a></li>
-        <li><a href="contact_us.php">Contact</a></li>
-        <li><a href="login.php">Login</a></li>
-      </ul>
-    </nav>
-  </div>
-</div>
-  <div id="menuBtn">
-    <img src="img/menu.png" id="menu">
-  </div>
-  
-
-<script>
-    var menuBtn=document.getElementById("menuBtn")
-    var sideNav=document.getElementById("sideNav")
-    var menu=document.getElementById("menu")
-
-        menuBtn.onclick=function(){
-          if(sideNav.style.right=="-250px"){
-            sideNav.style.right="0";
-            }
-          else{
-            sideNav.style.right="-250px";
-            
-          }
-}
-  </script>
 
 </body>
 </html>
