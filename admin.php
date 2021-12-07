8cc17e76ff6a556f825a6c7d4770347ad5fffd77
Shamim <74086860+Deathshotterdie98@users.noreply.github.com>
2021-12-08 00:09:04 +0600

    Update admin.php

--- admin.php
+++ admin.php
@@ -5,47 +5,19 @@ session_start();
 <!DOCTYPE html>
 <html>
 <head>
-	<meta charset="utf-8">
-	<title></title>
-	
-  <div class="banner-text">
-  <link rel="stylesheet" type="text/css" href="css/admin.css">
+
+  <div class="topnav">
+  <a class="active" href="index.php">Home</a>
+  <a href="New%20addition/contact_us_email_enabled/contact_us.php">Contact</a>
+  <a href="about_us.php">About Us</a>
+  <a href="logout.php">Log out</a>
+</div>
+
+  <link rel="stylesheet" type="text/css" href="css/ad.css">
 </head>
 <body>
+ 
 
-	<div class="banner-btn">
-  <div id="sideNav">
-    <nav>
-      <ul>
-        <li><a href="index.php">Home</a></li>
-        <li><a href="about_us.php">About Us</a></li>
-        <li><a href="contact_us.php">Contact</a></li>
-        <li><a href="login.php">Login</a></li>
-        <li><a href="logout.php">Logout</a></li>
-      </ul>
-    </nav>
-  </div>
-</div>
-  <div id="menuBtn">
-    <img src="img/menu.png" id="menu">
-  </div>
-  
-
-<script>
-    var menuBtn=document.getElementById("menuBtn")
-    var sideNav=document.getElementById("sideNav")
-    var menu=document.getElementById("menu")
-
-        menuBtn.onclick=function(){
-          if(sideNav.style.right=="-250px"){
-            sideNav.style.right="0";
-            }
-          else{
-            sideNav.style.right="-250px";
-            
-          }
-}
-  </script>
 
 </body>
 </html>
